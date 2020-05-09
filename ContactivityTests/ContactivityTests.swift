44280f65d8da9f61f82fdcf6718824edc6c432ee
maitreebain <maitreebain@gmail.com>
2020-05-08 23:04:14 -0400

    basic files added

---
+++ ContactivityTests/ContactivityTests.swift
@@ -0,0 +1,34 @@
+//
+//  ContactivityTests.swift
+//  ContactivityTests
+//
+//  Created by Maitree Bain on 5/8/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import XCTest
+@testable import Contactivity
+
+class ContactivityTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
