51a06423d0f17f78576ceca79b4ac3f21b717c29
maitreebain <maitreebain@gmail.com>
2020-05-16 23:31:48 -0400

    added database service and data per

---
+++ Contactivity/DataPersistence.swift
@@ -0,0 +1,9 @@
+//
+//  DataPersistence.swift
+//  Contactivity
+//
+//  Created by Maitree Bain on 5/16/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import Foundation
---
+++ Contactivity/DatabaseService.swift
@@ -0,0 +1,9 @@
+//
+//  DatabaseService.swift
+//  Contactivity
+//
+//  Created by Maitree Bain on 5/16/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import Foundation
